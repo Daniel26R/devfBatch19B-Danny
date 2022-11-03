4d6f42134b56cfbfab925844aecc5a55ddccea59
kikemaya <enrique.mg.cd@gmail.com>
2022-11-02 21:44:48 -0600

    closures e inicio del proyecto crud

---
+++ crud/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Capturando los nodos de html con javascript
+const app = document.querySelector(".app");
+
+const input_todo = document.getElementById("input-todo");
+const btn_add = document.getElementById("btn-add");
+const content_w = document.getElementById("content-wrapper");
+
+const btn_background = document.querySelector(".btn-background");
